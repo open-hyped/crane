7b8a2bcebadc3b49eb4fc37050c3251c931abf34
Niclas Doll <niclas.doll@icloud.com>
2024-12-25 13:34:33 +0100

    Implement lazy loading

--- src/crane/__init__.py
+++ src/crane/__init__.py
@@ -8,9 +8,31 @@ __all__ = [
     "DatasetConsumer",
     "ShardingStrategy",
     "TqdmReporterCallback",
+    "JsonDatasetWriter",
+    "ArrowDatasetWriter",
     "setup_logging",
 ]
 
+from typing import TYPE_CHECKING
 
 from .core import Callback, DatasetConsumer, ShardingStrategy, TqdmReporterCallback
 from .logging.setup import setup_logging
+
+if TYPE_CHECKING:
+    from .arrow import ArrowDatasetWriter
+    from .json import JsonDatasetWriter
+
+else:
+    import importlib
+    import sys
+
+    def lazy_load(name: str) -> object:
+        """Lazy loads module content based on attribute name."""
+        module = {
+            "JsonDatasetWriter": ".json",
+            "ArrowDatasetWriter": ".arrow",
+        }[name]
+        module = importlib.import_module(module, package=__name__)
+        return getattr(module, name)
+
+    sys.modules[__name__].__getattr__ = lazy_load
