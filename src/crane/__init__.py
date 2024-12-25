ca9d355812106dac18ca0c0f0ef7f60023c92a58
Niclas Doll <niclas.doll@icloud.com>
2024-12-25 13:08:19 +0100

    Port code from hyped.io.writers

--- src/crane/__init__.py
+++ src/crane/__init__.py
@@ -1,1 +1,16 @@
-A = 42
+"""Crane.
+
+Lift (process) and place (write) data streams, seamlessly and in parallel.
+"""
+
+__all__ = [
+    "Callback",
+    "DatasetConsumer",
+    "ShardingStrategy",
+    "TqdmReporterCallback",
+    "setup_logging",
+]
+
+
+from .core import Callback, DatasetConsumer, ShardingStrategy, TqdmReporterCallback
+from .logging.setup import setup_logging
